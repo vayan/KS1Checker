a243d1295d5969ca19f2d17254cf9e85c3db254f
Yann Vaillant <yann@vaillant.im>
2014-07-19 01:08:02 +0200

    get value for sk1 kimsufi

---
+++ kim.rb
@@ -0,0 +1,29 @@
+#!/user/bin/ruby -w
+# -*- coding: utf-8 -*-
+
+require 'net/http'
+require 'openssl'
+require 'json'
+
+http = Net::HTTP.new("ws.ovh.com", 443)
+http.use_ssl = true
+http.read_timeout = 1000
+
+#bad bad
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+response = http.request(Net::HTTP::Get.new("/dedicated/r2/ws.dispatcher/getAvailability2"))
+
+jsonbody = JSON.parse(response.body)["answer"]["availability"]
+
+jsonbody.each do|serv|
+  if serv["reference"] == "142sk1"
+    puts serv["zones"]
+
+  end
+end
+
+#puts(jsonbody["answer"]["__class"])
+
+
+__END__
