a236457714539629c1703b735a56918e120b8d54
Yann Vaillant <yann@vaillant.im>
2014-07-19 01:34:23 +0200

    fix strange formating

--- kim.rb
+++ kim.rb
@@ -1,24 +1,22 @@
-#!/user/bin/ruby -w
-# -*- coding: utf-8 -*-
-
-require 'net/http'
-require 'openssl'
-require 'json'
-
-@http = Net::HTTP.new("ws.ovh.com", 443)
-@http.use_ssl = true
-@http.read_timeout = 1000
-@http.verify_mode = OpenSSL::SSL::VERIFY_NONE #bad bad
+#!/user/bin/ruby -w
+# -*- coding: utf-8 -*-
+
+require 'net/http'
+require 'openssl'
+require 'json'
+
+@http = Net::HTTP.new("ws.ovh.com", 443)
+@http.use_ssl = true
+@http.read_timeout = 1000
+@http.verify_mode = OpenSSL::SSL::VERIFY_NONE #bad bad
 
 def is_sk1_avlb() 
 	puts "Start Checking OVH"
 	response = @http.request(Net::HTTP::Get.new("/dedicated/r2/ws.dispatcher/getAvailability2"))
-
 	jsonbody = JSON.parse(response.body)["answer"]["availability"]
 
 	jsonbody.each do|serv|
 		if serv["reference"] == "142sk1"
-
 			serv["zones"].each do |sk1|
 				puts sk1["zone"] + "is " + sk1["availability"]
 				if sk1["availability"] != "unavailable"
@@ -30,9 +28,6 @@ def is_sk1_avlb()
 	return false
 end
 
-
-#puts(jsonbody["answer"]["__class"])
-
 while true 
 	if is_sk1_avlb
 		puts "OMG" 
@@ -41,6 +36,6 @@ while true
 	end
 	sleep(2*60)
 end
-
-
-__END__
+
+__END__
+
