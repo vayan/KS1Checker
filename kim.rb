3e46fe897b662d4269638eb4bcfe5414f97813b7
Yann Vaillant <yann@vaillant.im>
2014-07-19 01:27:57 +0200

    add loop and timer

--- kim.rb
+++ kim.rb
@@ -5,25 +5,42 @@ require 'net/http'
 require 'openssl'
 require 'json'
 
-http = Net::HTTP.new("ws.ovh.com", 443)
-http.use_ssl = true
-http.read_timeout = 1000
-
-#bad bad
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-response = http.request(Net::HTTP::Get.new("/dedicated/r2/ws.dispatcher/getAvailability2"))
-
-jsonbody = JSON.parse(response.body)["answer"]["availability"]
-
-jsonbody.each do|serv|
-  if serv["reference"] == "142sk1"
-    puts serv["zones"]
+@http = Net::HTTP.new("ws.ovh.com", 443)
+@http.use_ssl = true
+@http.read_timeout = 1000
+@http.verify_mode = OpenSSL::SSL::VERIFY_NONE #bad bad
 
-  end
-end
-
+def is_sk1_avlb() 
+	puts "Start Checking OVH"
+	response = @http.request(Net::HTTP::Get.new("/dedicated/r2/ws.dispatcher/getAvailability2"))
+
+	jsonbody = JSON.parse(response.body)["answer"]["availability"]
+
+	jsonbody.each do|serv|
+		if serv["reference"] == "142sk1"
+
+			serv["zones"].each do |sk1|
+				puts sk1["zone"] + "is " + sk1["availability"]
+				if sk1["availability"] != "unavailable"
+					return true
+				end
+			end
+		end
+	end
+	return false
+end
+
+
 #puts(jsonbody["answer"]["__class"])
+
+while true 
+	if is_sk1_avlb
+		puts "OMG" 
+	else
+		puts "Nope...nothing.."
+	end
+	sleep(2*60)
+end
 
 
 __END__
